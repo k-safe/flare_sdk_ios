e156b7d0fc93a120f62729ae996e3376c73b530b
Jeevan <jeevan@phoenixinnovate.co.uk>
2024-11-18 17:17:39 +0530

    removed access key in code

--- Swift-SampleApp/FlareSDKSample/SideEngineSample/Utils/AppConfig.swift
+++ Swift-SampleApp/FlareSDKSample/SideEngineSample/Utils/AppConfig.swift
@@ -16,12 +16,10 @@ import Foundation
 
 enum AppConfig {
     enum Keys {
-//        static let app_secret_key = "Secret key here"
-//        static let production_key = "Production key here"
-//        static let sandbox_key = "Sandbox key here"
-        static let app_secret_key = "EN7nPbKOc57COfYaPy66j8bXhlvOkrcX87c7mC76"
-        static let production_key = "4afb485e-a181-4ce7-98f6-38cfe1afc748"
-        static let sandbox_key = "b6dd8509-d50e-48cc-af9e-ce9dcd712132"
+        static let app_secret_key = "Secret key here"
+        static let production_key = "Production key here"
+        static let sandbox_key = "Sandbox key here"
+        
     }
 }
 
